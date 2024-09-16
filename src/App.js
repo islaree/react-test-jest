0173fff9ae6c7112e688ae9b314b3679df8e8864
daxchx <daxchx.v1@gmail.com>
2024-09-16 18:00:05 +0900

    add: new components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { Counter } from "./components/Counter";
+import { Input } from "./components/Input";
+import { Users } from "./components/Users";
+
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
+      <Counter />
+      <Input />
+      <Users />
     </div>
   );
 }
